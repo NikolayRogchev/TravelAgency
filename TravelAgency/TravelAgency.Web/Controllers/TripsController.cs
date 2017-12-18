58b4e0d16eb757109f4b17391cdc8490b9cb5580
Nikolay <rogchev@gmail.com>
2017-12-18 20:19:58 +0200

    Added create view and action

---
+++ TravelAgency/TravelAgency.Web/Controllers/TripsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelAgency.Common;
+using TravelAgency.Data;
+using TravelAgency.Web.ViewModels.Trip;
+
+namespace TravelAgency.Web.Controllers
+{
+    public class TripsController : Controller
+    {
+        private readonly TravelAgencyDbContext trips;
+        public TripsController(TravelAgencyDbContext trips)
+        {
+            this.trips = trips;
+        }
+
+        public IActionResult All()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateTripViewModel createTripViewModel)
+        {
+            return View();
+        }
+    }
+}
---
+++ TravelAgency/TravelAgency.Web/ViewModels/Trip/CreateTripViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelAgency.Common;
+using TravelAgency.Common.Mapping;
+
+namespace TravelAgency.Web.ViewModels.Trip
+{
+    public class CreateTripViewModel
+    {
+        public string Name { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = Messages.MaxLengthExceededMessage)]
+        public string Destination { get; set; }
+        [Range(1, 30)]
+        public int Duration { get; set; }
+        [Required]
+        [Range(0, int.MaxValue)]
+        public decimal Price { get; set; }
+        [Range(0, 200)]
+        public int Capacity { get; set; }
+        public List<string> SignedUsers { get; set; }
+        public string Company { get; set; }
+    }
+}
